ec0d7549b783b52b4edd7316756842f387237335
IlkaMatsimnbe <ilkamatsimbe6@gmail.com>
2025-05-02 16:38:24 +0200

    Adcionei novas funcionalidades em todo codigo

--- app.js
+++ app.js
@@ -108,8 +108,47 @@ menuItems.forEach((item, index) => {
     currentProductTitle.textContent = choosenProduct.title;
     currentProductPrice.textContent = "$" + choosenProduct.price;
     currentProductImg.src = choosenProduct.colors[0].img;
+
+
+    //assign new colors
+    currentProductColors.forEach((color, index)=> {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+//Funcao para mudar de cor do produto a cada click no botao de cores
+currentProductColors.forEach((color, index)=>{
+  color.addEventListener("click", () =>{
+    currentProductImg.src = choosenProduct.colors[index].img
   });
 });
 
 
+//Funcao pra mudar de cor a cada click no botao de tamanho
+currentProductSizes.forEach((size, index)=>{
+  size.addEventListener("click", () =>{
+    currentProductSizes.forEach((size)=>{
+    size.style.backgroundColor = "white"
+    size.style.color = "black"
+    });
 
+    size.style.backgroundColor = "black"
+    size.style.color = "white"
+  });
+});
+
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+
+productButton.addEventListener ("click",()=>{
+  payment.style.display="flex"
+});
+
+
+close.addEventListener ("click", ()=>{
+  payment.style.display="none"
+});
